f3afc867d38d17487efd18bff6c6b05802901ba0
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-04-23 20:28:06 +0300

    feat: add vibration services

--- Assets/Code/Infrastructure/AudioVibrationFX/Services/StaticData/AudioVibrationStaticDataService.cs
+++ Assets/Code/Infrastructure/AudioVibrationFX/Services/StaticData/AudioVibrationStaticDataService.cs
@@ -6,14 +6,18 @@ namespace Code.Infrastructure.AudioVibrationFX.Services.StaticData
     public class AudioVibrationStaticDataService : IAudioVibrationStaticDataService
     {
         private const string SoundsDataPath = "StaticData/Sounds/Sounds";
+        private const string VibrationDataPath = "StaticData/Vibration/VibrationsData";
 
         private SoundsData _soundsData;
+        private VibrationsData _vibrationData;
         
         public SoundsData SoundsData => _soundsData;
+        public VibrationsData VibrationsData => _vibrationData;
 
         public void LoadData()
         {
             _soundsData = Resources.Load<SoundsData>(SoundsDataPath);
+            _vibrationData = Resources.Load<VibrationsData>(VibrationDataPath);
         }
     }
 }
--- Assets/Code/Infrastructure/AudioVibrationFX/Services/StaticData/IAudioVibrationStaticDataService.cs
+++ Assets/Code/Infrastructure/AudioVibrationFX/Services/StaticData/IAudioVibrationStaticDataService.cs
@@ -5,6 +5,7 @@ namespace Code.Infrastructure.AudioVibrationFX.Services.StaticData
     public interface IAudioVibrationStaticDataService
     {
         SoundsData SoundsData { get; }
+        VibrationsData VibrationsData { get; }
         void LoadData();
     }
 }
